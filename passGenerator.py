532c73660f603512651607481e0d864fdf9fde43
MisheninDev <misheninrb@yandex.ru>
2025-03-19 14:28:29 +0300

    added the choice of password length and the insertion of special characters into it

--- passGenerator.py
+++ passGenerator.py
@@ -1,9 +1,19 @@
 import random
 
 def genPass():
-    keyboard = '1234567890-_=+qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,<.>/?!@#$%^&*'
-    pasGen = random.sample(keyboard, 8)
-    result = ''.join(pasGen)
-    print(f'Ваш сгенерированный пароль: {result}')
+    keyboardSpec = '1234567890-_=+qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,<.>/?!@#$%^&*'
+    keyboardWOSpec  = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+    print('Доступные варианты пароля:\n1. С использованием специальных символов\n2. Без использования специальных символов')
+    choise = int(input('Выберите желаемый вариант пароля: '))
+    if choise == 1:
+        lenPassword = int(input('Введите желаемую длинну пароля: '))
+        pasGen = random.sample(keyboardSpec, lenPassword)
+        result = ''.join(pasGen)
+        print(f'Ваш сгенерированный пароль: {result}')
+    elif choise == 2:
+        lenPassword = int(input('Введите желаемую длинну пароля: '))
+        pasGen = random.sample(keyboardWOSpec, lenPassword)
+        result = ''.join(pasGen)
+        print(f'Ваш сгенерированный пароль: {result}') 
 
 genPass()
